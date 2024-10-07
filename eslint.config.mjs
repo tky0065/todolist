e66166d87946d5f6b986c942996ef1218e648e7e
tky0065 <tky0065@gmail.com>
2024-10-07 18:13:43 +0000

    Set up Jest for testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
- 
+
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
+    // ESLint plugin for Jest
+    {
+        files: ['**/*.test.ts'],
+        ...jestPlugin.configs['flat/recommended']
+    }
 );
---
+++ test/index.test.ts
@@ -0,0 +1,10 @@
+import {describe, expect,} from '@jest/globals';
+
+describe('#',()=>{
+    it('Should have an error message', () => {
+        // given
+        //when
+        //then
+        expect(true).toBe(true);
+    })
+})
